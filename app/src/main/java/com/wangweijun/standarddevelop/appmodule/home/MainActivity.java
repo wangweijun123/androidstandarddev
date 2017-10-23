c99f9a6581631e30ee41f6f87fdaa0545cd77b41
wangweijun <262448350@qq.com>
2017-10-24 00:35:34 +0800

    add glide for load image

--- app/src/main/java/com/wangweijun/standarddevelop/appmodule/home/MainActivity.java
+++ app/src/main/java/com/wangweijun/standarddevelop/appmodule/home/MainActivity.java
@@ -2,17 +2,25 @@ package com.wangweijun.standarddevelop.appmodule.home;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.wangweijun.standarddevelop.R;
 import com.wangweijun.standarddevelop.io.network.LeHttpApi;
 
 public class MainActivity extends Activity {
-
+    ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         LeHttpApi.listRepos();
+
+        iv = (ImageView)findViewById(R.id.iv);
+
+        Glide.with(getApplicationContext())
+                .load("http://i1.letvimg.com//lc05_iptv//201709//19//17//20//tmp3e3e95cf-3bf4-46c1-a36a-95ca33010891256.png")
+                .into(iv);
     }
 }
